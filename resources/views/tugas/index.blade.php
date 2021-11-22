46afda09e31d673c52f52d561bf9ab15b3619450
Naufal Sadewa <90032641+naufalsad3@users.noreply.github.com>
2021-11-22 18:41:50 +0700

    updateTabelLaravel

---
+++ resources/views/tugas/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Penugasan Tabel Laravel - 5026201136</title>
+</head>
+<body>
+
+	<h2>Naufal Sadewa (5026201136) / PWEB B</h2>
+	<h3>Data Tugas</h3>
+
+	<a href="/tugas/tambah"> + Tambah Pegawai Baru</a>
+
+	<br/>
+	<br/>
+
+	<table border="1">
+		<tr>
+			<th>ID Pegawai</th>
+			<th>Tanggal</th>
+			<th>Nama Tugas</th>
+			<th>Status</th>
+		</tr>
+		@foreach($tugas as $t)
+		<tr>
+			<td>{{ $t->IDPegawai }}</td>
+			<td>{{ $t->Tanggal }}</td>
+			<td>{{ $t->NamaTugas }}</td>
+			<td>{{ $t->Status }}</td>
+			<td>
+				<a href="/tugas/edit/{{ $t->ID }}">Edit</a>
+				|
+				<a href="/tugas/hapus/{{ $t->ID }}">Hapus</a>
+			</td>
+		</tr>
+		@endforeach
+	</table>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,8 @@ Route::get('php', 'ViewController@showphppweb');
 
 Route::post('tgsphp', 'ViewController@showtgsphppweb');
 
+
+
 Route::get('/pegawai','PegawaiController@index');
 
 Route::get('/pegawai/tambah','PegawaiController@tambah');
@@ -42,3 +44,17 @@ Route::get('/pegawai/edit/{id}','PegawaiController@edit');
 Route::post('/pegawai/update','PegawaiController@update');
 
 Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
+
+
+Route::get('/tugas','TugasController@index');
+
+Route::get('/tugas/tambah','TugasController@tambah');
+
+Route::post('/tugas/store','TugasController@store');
+
+Route::get('/tugas/edit/{id}','TugasController@edit');
+
+Route::post('/tugas/update','TugasController@update');
+
+Route::get('/tugas/hapus/{id}','TugasController@hapus');
